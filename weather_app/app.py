d097e1f9dbba1c7e5eb7602a1efde0ddcf5d763e
djedd1ne <mekki.djallaleddine@gmail.com>
2025-12-10 17:30:58 -0500

    handle errors using response status code and cod

--- weather_app/app.py
+++ weather_app/app.py
@@ -5,30 +5,38 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-from dotenv import load_dotenv
-
-#Load .env
+# Load .env
 load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 if API_KEY:
-    print ("API key loaded successfully.")
+    print("API key loaded successfully.")
 else:
     print("Error: API_KEY not found.")
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+
 def fetch_weather(city):
     params = {
-        "q" : city,
+        "q": city,
         "appid": API_KEY,
         "units": "metric"
     }
     response = requests.get(BASE_URL, params=params)
-    #https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API_key}&units=metrics
-    if response.status_code == 200:
-        return response.json()
+    
+    # http request failed
+    if response.status_code != 200:
+        return {"success": False, "error": response.json().get("message")}
+    
+    # http request succeeded check cod
+    data = response.json()
+    cod = str(data.get("cod"))
+    
+    if cod == "200":
+        return {"success": True, "data": data}
     else:
-        return None
+        return {"success": False, "error": data.get("message")}
+
 
 def parse_weather(data):
     return {
@@ -39,15 +47,21 @@ def parse_weather(data):
         "wind_speed": data["wind"]["speed"]
     }
 
+
 @app.route('/', methods=["GET", "POST"])
 def home():
     weather = None
+    error = None
+
     if request.method == "POST":
         city = request.form.get("city")
-        data = fetch_weather(city)
-        if data:
-            weather = parse_weather(data)
-    return render_template("index.html", weather=weather)
+        result = fetch_weather(city)
+        if result["success"]:
+            weather = parse_weather(result["data"])
+        else:
+            error = result["error"]
+    return render_template("index.html", weather=weather, error=error)
+
 
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug=True)
